e6233aae400c5050ccc6df633b191e9774dbbf0a
Shawn Balcome <balc0022@umn.edu>
2016-12-01 18:24:03 -0600

    Safety commit, started playing with roxygen2

--- R/opcbinomial.R
+++ R/opcbinomial.R
@@ -1,3 +1,18 @@
+#' Illustration of crayon colors
+#'
+#' Creates a plot of the crayon colors in \code{\link{brocolors}}
+#'
+#' @param method2order method to order colors (\code{"hsv"} or \code{"cluster"})
+#' @param cex character expansion for the text
+#' @param mar margin paramaters; vector of length 4 (see \code{\link[graphics]{par}})
+#'
+#' @return None
+#'
+#' @examples
+#' plot_crayons()
+#'
+#' @export
+
 library(shiny)
 library(shinyFiles)
 library(shinythemes)
