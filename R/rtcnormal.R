3eb14732ee4b32c95da32fd3584c42eddbda913b
balc0022 <balcomes@gmail.com>
2016-12-04 20:09:11 -0600

    More S4 refactoring 2

--- R/rtcnormal.R
+++ R/rtcnormal.R
@@ -2,38 +2,48 @@
 # Create the first quadrant class
 #
 # This is used to represent a coordinate in the first quadrant.
-FirstQuadrant <- setClass(
+rtcnormal <- setClass(
   # Set the name for the class
-  "FirstQuadrant",
+  "rtcnormal",
 
   # Define the slots
   slots = c(
-    ui = "numeric",
-    server = "numeric"
-  ),
+    ui = "character",
+    server = "function"
+  )#,
 
   # Set the default values for the slots. (optional)
-  prototype=list(
-    x = 0.0,
-    y = 0.0
-  ),
+  #prototype=list(
+  #  x = 0.0,
+  #  y = 0.0
+  #),
 
   # Make a function that can test to see if the data is consistent.
   # This is not called if you have an initialize function defined!
-  validity=function(object)
-  {
-    if((object@x < 0) || (object@y < 0)) {
-      return("A negative number for one of the coordinates was given.")
-    }
-    return(TRUE)
-  }
+  #validity=function(object)
+  #{
+  #  if((object@x < 0) || (object@y < 0)) {
+  #    return("A negative number for one of the coordinates was given.")
+  #  }
+  #  return(TRUE)
+  #}
 )
+
+setGeneric(name="shine",
+           def=function(x)
+           {
+             standardGeneric("shine")
+           }
+)
+
+setMethod(f="shine",
+          signature="rtcnormal ",
+          definition=function(x)
+          {
 
 ################################################################################
 # UI
 ################################################################################
-
-options(shiny.launch.browser = .rs.invokeShinyWindowViewer)
 
 ui <- shinyUI(pageWithSidebar(
   # Application title
@@ -213,4 +223,8 @@ server <- shinyServer(function(input, output, session){
     res1()$prior_for_control_group
   })
 })
+
+pane_runApp(ui,server)
+          }
+)
 
