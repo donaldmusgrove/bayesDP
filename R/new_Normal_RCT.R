fc2b49a6e6e7c7d016fe9b5fcdb1e6c816c32aae
balc0022 <balcomes@gmail.com>
2016-12-06 21:56:33 -0600

    New normal rtc

---
+++ R/new_Normal_RCT.R
@@ -0,0 +1,6 @@
+normalrtc(test,control)
+
+
+loss_function_inputs,test_group_info,control_group_info,number_mcmc
+
+loss_function_inputs,test_group_info,control_group_info,two_sided_loss_function = False, number_mcmc=1000
