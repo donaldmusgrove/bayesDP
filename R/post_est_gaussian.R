8a594bf1229f8ef76aa06778e4a059997f8307ff
balc0022 <balcomes@gmail.com>
2016-12-28 19:53:44 -0600

    Rolled back changes, put alternative in holding cell folder.

--- R/post_est_gaussian.R
+++ R/post_est_gaussian.R
@@ -60,24 +60,29 @@
 #     prior.df    = c(1,1))
 
 setGeneric("post_est_gaussian",
-           function(data, ...){
+           function(data,
+                    formula = y ~ treatment + x,
+                    family = "gaussian",
+                    treatment = NULL,
+                    prior.dist = NULL,
+                    prior.mean = 0,
+                    prior.scale = 1000,
+                    prior.df = Inf,
+                    prior_mu = 1,
+                    prior_sigma = 0.1,
+                    weibull_scale = 1,
+                    weibull_shape = 1,
+                    alpha_max = 1,
+                    N0_max = NULL,
+                    N_mcmc = 10000){
              standardGeneric("post_est_gaussian")
-             })
+           })
 
-setMethod("post_est_gaussian",
-          signature(prior.mean = "numeric",
-                    prior.scale = "numeric",
-                    prior.df = "numeric"),
-          function(prior.mean = 0,
-                   prior.scale = 1000,
-                   prior.df = Inf){
-            message("This is a case where the user doesn't have the data and
-                    submits a list of parameters instead, a dataframe is build
-                    and then submitted to the dataframe method")
-            historical <- data.frame(y         = rnorm(100, 4, 0.1),
-                                     x         = c(rnorm(50,1,0.1), rnorm(50,3,0.1)),
-                                     treatment = c(rep(0,50),rep(1,50)))
-          })
+#setMethod("post_est_gaussian",
+#          signature(data = "list"),
+#          function(data){
+#            message("This is a case where the user doesn't have the data")
+#          })
 
 setMethod("post_est_gaussian",
           signature(data = "missing"),
@@ -86,24 +91,21 @@ setMethod("post_est_gaussian",
           })
 
 setMethod("post_est_gaussian",
-          signature(prior.mean = "missing",
-                    prior.scale = "missing",
-                    prior.df = "missing"),
-                    #data = "data.frame"),
-                    #formula = "formula",
-                    #family = "character",
-                    #treatment = "character",
-                    #prior.dist = "character",
-                    #prior.mean = "numeric",
-                    #prior.scale = "numeric",
-                    #prior.df = "numeric",
-                    #prior_mu = "numeric",
-                    #prior_sigma = "numeric",
-                    #weibull_scale = "numeric",
-                    #weibull_shape = "numeric",
-                    #alpha_max = "numeric",
-                    #N0_max = "missing",
-                    #N_mcmc = "numeric"),
+          signature(data = "data.frame"),
+          #formula = "formula",
+          #family = "character",
+          #treatment = "character",
+          #prior.dist = "character",
+          #prior.mean = "numeric",
+          #prior.scale = "numeric",
+          #prior.df = "numeric",
+          #prior_mu = "numeric",
+          #prior_sigma = "numeric",
+          #weibull_scale = "numeric",
+          #weibull_shape = "numeric",
+          #alpha_max = "numeric",
+          #N0_max = "missing",
+          #N_mcmc = "numeric"),
           function(data,
                    formula = y ~ treatment + x,
                    family = "gaussian",
@@ -120,72 +122,72 @@ setMethod("post_est_gaussian",
                    N0_max = NULL,
                    N_mcmc = 10000){
 
-N_new        <- nrow(data)
+            N_new        <- nrow(data)
 
-################################################################################
-### Estimate alpha (loss function)
-################################################################################
-q_flat       <- bayesglm(formula,
-                         prior.df    = prior.df,
-                         prior.scale = prior.scale,
-                         prior.mean  = prior.mean,
-                         family      = family,
-                         data        = data)
+            ################################################################################
+            ### Estimate alpha (loss function)
+            ################################################################################
+            q_flat       <- bayesglm(formula,
+                                     prior.df    = prior.df,
+                                     prior.scale = prior.scale,
+                                     prior.mean  = prior.mean,
+                                     family      = family,
+                                     data        = data)
 
-### Grab mean/sd of treatment effect
-mu_new      <- summary(q_flat)$coefficients[2,1]
-sd_new      <- summary(q_flat)$coefficients[2,2]
+            ### Grab mean/sd of treatment effect
+            mu_new      <- summary(q_flat)$coefficients[2,1]
+            sd_new      <- summary(q_flat)$coefficients[2,2]
 
-### Compare new data to "historical data"
-mc_new_data  <- rnorm(N_mcmc,mu_new,sd_new)
-mc_hist_data <- rnorm(N_mcmc, prior_mu, prior_sigma)
+            ### Compare new data to "historical data"
+            mc_new_data  <- rnorm(N_mcmc,mu_new,sd_new)
+            mc_hist_data <- rnorm(N_mcmc, prior_mu, prior_sigma)
 
-### Compare distributions of current and historical data
-p           <- mean(mc_hist_data<mc_new_data)
-p_test1     <- ifelse(p > 0.5, 1 - p, p)
+            ### Compare distributions of current and historical data
+            p           <- mean(mc_hist_data<mc_new_data)
+            p_test1     <- ifelse(p > 0.5, 1 - p, p)
 
-### Compute alpha0, loss function, from Weibull loss function
-alpha0      <- pweibull(p_test1, shape = weibull_shape, scale = weibull_scale)*alpha_max
-alpha0      <- ifelse(alpha0<1e-12,1e-12,alpha0)
+            ### Compute alpha0, loss function, from Weibull loss function
+            alpha0      <- pweibull(p_test1, shape = weibull_shape, scale = weibull_scale)*alpha_max
+            alpha0      <- ifelse(alpha0<1e-12,1e-12,alpha0)
 
-### Scale the standard deviation of the historical group by alpha0
-std0        <- prior_sigma/sqrt(alpha0)
+            ### Scale the standard deviation of the historical group by alpha0
+            std0        <- prior_sigma/sqrt(alpha0)
 
-### Note: if N0_max is not null, then use
-if(!is.null(N0_max)){
-  alpha0     <- pweibull(p_test1, shape = weibull_shape, scale = weibull_scale)
-  alpha0     <- ifelse(alpha0<1e-12,1e-12,alpha0)
-  N_eff      <- N0_max*alpha0
-  std0       <- prior_sigma/sqrt(N0_max)
-}
+            ### Note: if N0_max is not null, then use
+            if(!is.null(N0_max)){
+              alpha0     <- pweibull(p_test1, shape = weibull_shape, scale = weibull_scale)
+              alpha0     <- ifelse(alpha0<1e-12,1e-12,alpha0)
+              N_eff      <- N0_max*alpha0
+              std0       <- prior_sigma/sqrt(N0_max)
+            }
 
-################################################################################
-### Estimate treatment effect of current data with historical prior,
-### where historical prior has been down-weighted using alpha0
-################################################################################
-q_final     <- bayesglm(formula,
-                        prior.df    = prior.df,
-                        prior.scale = c(std0, prior.scale),
-                        prior.mean  = c(prior_mu, prior.mean),
-                        family      = gaussian,
-                        data        = data)
+            ################################################################################
+            ### Estimate treatment effect of current data with historical prior,
+            ### where historical prior has been down-weighted using alpha0
+            ################################################################################
+            q_final     <- bayesglm(formula,
+                                    prior.df    = prior.df,
+                                    prior.scale = c(std0, prior.scale),
+                                    prior.mean  = c(prior_mu, prior.mean),
+                                    family      = gaussian,
+                                    data        = data)
 
-### Extract posterior estimates of intercept, treatment effect, and covariate x
-mu_post       <- summary(q_final)$coefficients[,1]
-sd_post       <- vcov(q_final)
+            ### Extract posterior estimates of intercept, treatment effect, and covariate x
+            mu_post       <- summary(q_final)$coefficients[,1]
+            sd_post       <- vcov(q_final)
 
-### Draw posterior samples of the regression estimates
-### - This is a Laplace approximation - we will derive one other option
-mc_final_data <- mvrnorm(N_mcmc, mu_post, sd_post)
+            ### Draw posterior samples of the regression estimates
+            ### - This is a Laplace approximation - we will derive one other option
+            mc_final_data <- mvrnorm(N_mcmc, mu_post, sd_post)
 
-out <- list(alpha0           = alpha0,
-            bayesian_p_value = p,
-            N_new            = N_new,
-            mu_post          = mu_post,
-            sd_post          = sd_post,
-            mc_final_data    = mc_final_data,
-            mc_hist_data     = mc_hist_data,
-            mc_new_data      = mc_new_data,
-            effect_est       = coef(q_final)[2])
+            out <- list(alpha0           = alpha0,
+                        bayesian_p_value = p,
+                        N_new            = N_new,
+                        mu_post          = mu_post,
+                        sd_post          = sd_post,
+                        mc_final_data    = mc_final_data,
+                        mc_hist_data     = mc_hist_data,
+                        mc_new_data      = mc_new_data,
+                        effect_est       = coef(q_final)[2])
 
-return(out)})
+            return(out)})
