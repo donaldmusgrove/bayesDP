5bb559c4c0705b0d185d9a15cfbf4840e456df05
balc0022 <balcomes@gmail.com>
2017-01-19 12:59:24 -0600

    Continuing data_helper

--- R/data_helper.R
+++ R/data_helper.R
@@ -1,42 +1,10 @@
-x <- airquality[, -1] # x is a regression design matrix
-y <- airquality[,  1] # y is the corresponding response
-
-stopifnot(complete.cases(y) != is.na(y))
-ok <- complete.cases(x, y)
-sum(!ok) # how many are not "ok" ?
-x <- x[ok,]
-y <- y[ok]
-
-sapply(current,mean)
-sapply(historical,sd)
-
-
 #' data_helper
 #'
 #' data_helper
 #'
 #' @title data_helper: data_helper
-#' @param mu_t numeric
-#' @param sigma2_t numeric
-#' @param N_t numeric
-#' @param mu_c numeric
-#' @param sigma2_c numeric
-#' @param N_c numeric
-#' @param mu0_t numeric
-#' @param sigma02_t numeric
-#' @param N0_t numeric
-#' @param mu0_c numeric
-#' @param sigma02_c numeric
-#' @param N0_c numeric
-#' @param type character
-#' @param alpha_max numeric
-#' @param weibull_scale numeric
-#' @param weibull_shape numeric
-#' @param number_mcmc numeric
-#' @param two_side character
-#' @param inequality character
-#' @param delta numeric
-#'
+#' @param current dataframe
+#' @param historical dataframe
 #'
 #' @examples
 #'
@@ -50,25 +18,37 @@ setGeneric("data_helper",
            })
 
 setMethod("data_helper",
-          signature(historical = "missing"),
-          function(type = c("1arm", "2arm"), ...){
-            if (type == "1arm"){
-              return(data_helper1arm(...))
-            }
-            if(type == "2arm"){
-              return(data_helper2arm(...))
-            }
+          signature(current = "data.frame", historical = "missing"),
+          function(current, historical){
+
+            ok <- current[complete.cases(current),]
+
+            n <- nrow(ok)
+            mu <- sapply(ok, mean)
+            #sigma <- sapply(ok, sd)
+            sigma2 <- sapply(ok, var)
+            return(list(n,mu,sigma2))
+
           })
 
 setMethod("data_helper",
-          signature(historical = "missing"),
-          function(type = c("1arm", "2arm"), ...){
-            if (type == "1arm"){
-              return(data_helper1arm(...))
-            }
-            if(type == "2arm"){
-              return(data_helper2arm(...))
-            }
+          signature(current = "data.frame", historical = "data.frame"),
+          function(current, historical){
+            ok <- current[complete.cases(current),]
+
+            n <- nrow(ok)
+            mu <- sapply(ok, mean)
+            #sigma <- sapply(ok, sd)
+            sigma2 <- sapply(ok, var)
+
+            ok0 <- current[complete.cases(historical),]
+
+            n0 <- nrow(ok0)
+            mu0 <- sapply(ok0, mean)
+            #sigma <- sapply(ok, sd)
+            sigma02 <- sapply(ok0, var)
+
+            return(list(n,mu,sigma2,n0,mu0,sigma02))
           })
 
 
