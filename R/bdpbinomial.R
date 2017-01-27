248ab9aac6ec7d35a932e0614603516ccad9cf24
Donnie M <donniemusgrove@gmail.com>
2017-01-27 14:08:18 -0600

    Roxygen clean up

--- R/bdpbinomial.R
+++ R/bdpbinomial.R
@@ -12,12 +12,12 @@
 #' @param y0_c Number of events for the historical control group.
 #' @param N0_c Sample size of the historical control group.
 #' @param type One of "1arm" or "2arm", denoting an OPC trial or a randomized control trial(RCT), respectively.
-#' @param alpha_max Maximum weight the discount function can apply. Default is 1. 
+#' @param alpha_max Maximum weight the discount function can apply. Default is 1. For type="2arm", users may specify a vector of two values where the first value is used to weight the historical treatment group and the second value is used to weight the historical control group.
 #' @param a0 Prior value for the beta rate. Default is 1.
 #' @param b0 Prior value for the beta rate. Default is 1.
 #' @param number_mcmc Number of Markov Chain Monte Carlo (MCMC) simulations. Default is 1e4.
-#' @param weibull_shape Shape parameter of the Weibull discount function used to compute alpha, the weight parameter of the historical data. Default value is 3.
-#' @param weibull_scale Scale parameter of the Weibull discount function used to compute alpha, the weight parameter of the historical data. Default value is 0.135. Two values have special treatment: 0 and Inf. For weibull_scale = 0, alpha is set to 0, i.e., no weight. For weibull_scale = Inf, alpha is set to 1, i.e., full weight.
+#' @param weibull_shape Shape parameter of the Weibull discount function used to compute alpha, the weight parameter of the historical data. Default value is 3. For type="2arm", users may specify a vector of two values where the first value is used to estimate the weight of the historical treatment group and the second value is used to estimate the weight of the historical control group.
+#' @param weibull_scale Scale parameter of the Weibull discount function used to compute alpha, the weight parameter of the historical data. Default value is 0.135. Two values have special treatment: 0 and Inf. For weibull_scale = 0, alpha is set to 0, i.e., no weight. For weibull_scale = Inf, alpha is set to 1, i.e., full weight. For type="2arm", users may specify a vector of two values where the first value is used to estimate the weight of the historical treatment group and the second value is used to estimate the weight of the historical control group.
 #' @param two_side Indicator of two-sided test for the discount function. Default value is 1.
 #'
 #' @examples
@@ -52,12 +52,12 @@ discount_function_binomial <- function(y, N, y0, N0, alpha_max, a0, b0, number_m
     ### Theta for using flat prior
     a_post_flat     <- y + a0
     b_post_flat     <- N - y + b0
-    posterior_flat  <- rbeta(number_mcmc, a_post_flat, b_post_flat)
-
+    posterior_flat  <- rbeta(number_mcmc, a_post_flat, b_post_flat)  
+    
     ### Prior model
     a_prior  <- y0 + a0
     b_prior  <- N0 - y0 + b0
-    prior   <- rbeta(number_mcmc, a_prior, b_prior) #flat prior
+    prior    <- rbeta(number_mcmc, a_prior, b_prior) #flat prior
 
     ### Test of model vs real
     p_test <- mean(posterior_flat < prior)   # larger is higher failure
