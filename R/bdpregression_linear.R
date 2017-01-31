494c01616823ea1ad11760b5e4aa7adfab933893
balc0022 <balcomes@gmail.com>
2017-01-30 20:17:58 -0600

    Builds again

--- R/bdpregression_linear.R
+++ R/bdpregression_linear.R
@@ -1,14 +1,8 @@
-
-############################################################################
-### Need to fix plots: should not involve effective sample sizes, instead,
-### weight of alpha from 0 --> 1
-
-
-#' Post Estimate Gaussian
+#' bpdregression_linear
 #'
-#' Post Estimate Gaussian
+#' bpdregression_linear
 #'
-#' @title post_est_gaussian: Post Estimate Gaussian
+#' @title bdpregression_linear: Post Estimate Gaussian
 #' @param data numeric number
 #' @param formula formula
 #' @param family family
@@ -17,7 +11,7 @@
 #' @param sigma02 prior variance of the treatment effect.
 #' @param prior.dist prior distribution of the treatment and covariate effects.
 #' @param prior.mean prior mean of the covariate effects. Default is zero.
-#' @param prior.scale prior variance of the covariate effects. Default is 1e6. 
+#' @param prior.scale prior variance of the covariate effects. Default is 1e6.
 #' @param prior.df prior degrees of freedom of the treatment and covariate effects.
 #' @param weibull_scale weibull_scale
 #' @param weibull_shape weibull_shape
@@ -33,7 +27,7 @@
 #'                    x         = c(rnorm(50,1,0.1), rnorm(50,3,0.1)),
 #'                    treatment = c(rep(0,50),rep(1,50)))
 #'
-#' fit <- post_est_gaussian(data          = data,
+#' fit <- bdpregression_linear(data          = data,
 #'                          formula       = y ~ treatment + x,
 #'                          prior_mu      = 1,
 #'                          prior_sigma   = 0.1,
@@ -45,12 +39,17 @@
 #' ### Main parameter of interest:
 #' fit$effect_est
 #'
-#' @rdname post_est_gaussian
-#' @export post_est_gaussian
+#' @rdname bpdregression_linear
+#' @export bpdregression_linear
+
+
+############################################################################
+### Need to fix plots: should not involve effective sample sizes, instead,
+### weight of alpha from 0 --> 1
 
 
 
-setGeneric("post_est_gaussian",
+setGeneric("bdpregression_linear",
            function(data,
                     formula = y ~ treatment + x,
                     family = "gaussian",
@@ -68,22 +67,22 @@ setGeneric("post_est_gaussian",
                     two_side      = 0,
                     inequality    = "<",
                     delta         = 0){
-             standardGeneric("post_est_gaussian")
+             standardGeneric("bdpregression_linear")
            })
 
-#setMethod("post_est_gaussian",
+#setMethod("bdpregression_linear",
 #          signature(data = "list"),
 #          function(data){
 #            message("This is a case where the user doesn't have the data")
 #          })
 
-setMethod("post_est_gaussian",
+setMethod("bdpregression_linear",
           signature(data = "missing"),
           function(data){
             message("Missing object")
           })
 
-setMethod("post_est_gaussian",
+setMethod("bdpregression_linear",
           signature(data = "data.frame"),
           function(data,
                    formula = y ~ treatment + x,
@@ -105,7 +104,7 @@ setMethod("post_est_gaussian",
 
   N_new        <- nrow(data)
 
-  
+
   ### Check validity of family input
   call <- match.call()
   if (is.character(family)) {
@@ -118,12 +117,12 @@ setMethod("post_est_gaussian",
     print(family)
     stop("'family' not recognized")
   }
-  
+
   ### Pull data from the environment if data input is missing
   if (missing(data)) {
     data <- environment(formula)
   }
-  
+
   mf <- match.call(expand.dots = FALSE)
   m  <- match(c("formula", "data", "offset"), names(mf), 0L)
   mf <- mf[c(1L, m)]
@@ -139,36 +138,36 @@ setMethod("post_est_gaussian",
       names(Y) <- nm
     }
   }
-  
+
   X <- if (!is.empty.model(mt)) {
-    model.matrixBayes(object = mt, data = data, contrasts.arg = NULL, 
+    model.matrixBayes(object = mt, data = data, contrasts.arg = NULL,
                       keep.order = TRUE, drop.baseline = TRUE)
   } else {
     matrix(, NROW(Y), 0L)
   }
-  
+
   offset <- as.vector(model.offset(mf))
   if (!is.null(offset)) {
-    if (length(offset) != NROW(Y)) 
-      stop(gettextf("number of offsets is %d should equal %d (number of\n            observations)", 
+    if (length(offset) != NROW(Y))
+      stop(gettextf("number of offsets is %d should equal %d (number of\n            observations)",
                     length(offset), NROW(Y)), domain = NA)
   } else{
     offset <- numeric(NROW(Y))
   }
-            
-  
-  
+
+
+
   ################################################################################
   # Estimate alpha, discount function
   # - Currently, only place prior on (adjusted) treatment effect
   ################################################################################
-  Loss_function <- function(Y, X, offset, family, treatment, mu0, sigma02, alpha_max, 
-                            prior.df, prior.scale, prior.mean, 
+  Loss_function <- function(Y, X, offset, family, treatment, mu0, sigma02, alpha_max,
+                            prior.df, prior.scale, prior.mean,
                             number_mcmc, weibull_shape, weibull_scale, two_side){
-              
-    
+
+
     ### Get treatment effect estimate using flat prior
-    q_flat <- bayesglm.fit(x           = X, 
+    q_flat <- bayesglm.fit(x           = X,
                            y           = Y,
                            offset      = offset,
                            prior.df    = prior.df,
@@ -177,20 +176,20 @@ setMethod("post_est_gaussian",
                            family      = family)
     class(q_flat) <- c("bayesglm", "glm", "lm")
 
-    
+
     ### Grab mean/sd of treatment effect
     coef_table <- summary(q_flat)$coefficients
     coef_trt   <- which(row.names(coef_table) == treatment)
     mu_new     <- coef_table[coef_trt,1]
     sd_new     <- coef_table[coef_trt,2]
-    
-    
+
+
     ### Compare new data to "historical data"
     mu_post_flat  <- rnorm(number_mcmc, mu_new, sd_new)
     mu_post_flat0 <- rnorm(number_mcmc, mu0, sigma02)
-    
+
     ### Test of model vs real
-    p_test  <- mean(mu_post_flat < mu_post_flat0) 
+    p_test  <- mean(mu_post_flat < mu_post_flat0)
 
     ### Number of effective sample size given shape and scale loss function
     if (two_side == 0) {
@@ -199,14 +198,14 @@ setMethod("post_est_gaussian",
       p_test1    <- ifelse(p_test > 0.5, 1 - p_test, p_test)
       alpha_loss <- pweibull(p_test1, shape = weibull_shape, scale = weibull_scale) * alpha_max
     }
-    
+
     return(list(alpha_loss   = alpha_loss,
                 pvalue       = p_test,
                 mu_post_flat = mu_post_flat,
                 mu0          = mu_post_flat0))
   }
 
-  
+
   ### Scale the standard deviation of the historical group by alpha0
   #std0        <- prior_sigma/sqrt(alpha0)
 
@@ -215,12 +214,12 @@ setMethod("post_est_gaussian",
   # Estimate posterior for (adjusted) treatment effect given alpha_loss value
   ################################################################################
   mu_post_aug <- function(Y, X, offset, family, treatment, mu0, sigma02, alpha_loss,
-                          prior.df, prior.scale, prior.mean, 
+                          prior.df, prior.scale, prior.mean,
                           number_mcmc, weibull_shape, weibull_scale) {
 
     ### Scale the historicall variance with respect to the alpha value
     std0        <- sqrt(sigma02)/sqrt(alpha_loss)
-    
+
     ### Match historical treatment info to the correct slot
     nP                 <- ncol(X)-1
     namesX             <- colnames(X)[-1]
@@ -230,7 +229,7 @@ setMethod("post_est_gaussian",
     prior_scale        <- rep(prior.scale, nP)
     prior_scale[idTrt] <- std0^2
 
-    
+
     q_final  <- bayesglm.fit(x           = X,
                              y           = Y,
                              offset      = offset,
@@ -239,75 +238,75 @@ setMethod("post_est_gaussian",
                              prior.mean  = prior_mean,
                              family      = family)
     class(q_final) <- c("bayesglm", "glm", "lm")
-    
-    
+
+
     ### Grab mean/sd of treatment effect
     coef_table <- summary(q_final)$coefficients
     coef_trt   <- which(row.names(coef_table) == treatment)
     mu_new     <- coef_table[coef_trt,1]
     sd_new     <- coef_table[coef_trt,2]
 
-        
+
     ### Calculate posterior of the treatment effect
     mu_post <- rnorm(number_mcmc, mu_new, sd_new)
 
-    
+
     ### Extract posterior estimates of intercept, treatment effect, and covariate(s)
     mu_covariate_post <- summary(q_final)$coefficients[,1]
     sd_covariate_post <- vcov(q_final)
 
-    
+
     ### Draw posterior samples of the regression estimates
     ### - This is a Laplace approximation - we will derive one other option
     covariate_post <- MASS::mvrnorm(number_mcmc, mu_covariate_post, sd_covariate_post)
-    
-        
+
+
     return(list(mu_post           = mu_post,
                 mu_covariate_post = mu_covariate_post,
                 sd_covariate_post = sd_covariate_post,
                 covariate_post    = covariate_post))
   }
-  
-  
+
+
   ################################################################################
   # Combine loss function and posterior estimation into one function             #
   ################################################################################
-  mu_posterior <- function(Y, X, offset, family, treatment, mu0, sigma02, alpha_max, 
-                           prior.df, prior.scale, prior.mean, 
+  mu_posterior <- function(Y, X, offset, family, treatment, mu0, sigma02, alpha_max,
+                           prior.df, prior.scale, prior.mean,
                            number_mcmc, weibull_shape, weibull_scale, two_side) {
 
-    alpha_loss <- Loss_function(Y             = Y, 
-                                X             = X, 
-                                offset        = offset, 
-                                family        = family, 
-                                treatment     = treatment, 
-                                mu0           = mu0, 
-                                sigma02       = sigma02, 
-                                alpha_max     = alpha_max, 
-                                prior.df      = prior.df, 
-                                prior.scale   = prior.scale, 
-                                prior.mean    = prior.mean, 
-                                number_mcmc   = number_mcmc, 
-                                weibull_shape = weibull_shape, 
-                                weibull_scale = weibull_scale, 
-                                two_side      = two_side)
-      
-    mu_posterior <- mu_post_aug(Y             = Y, 
-                                X             = X, 
-                                offset        = offset, 
-                                family        = family, 
-                                treatment     = treatment, 
-                                mu0           = mu0, 
-                                sigma02       = sigma02, 
-                                alpha_loss    = alpha_loss$alpha_loss,
-                                prior.df      = prior.df, 
+    alpha_loss <- Loss_function(Y             = Y,
+                                X             = X,
+                                offset        = offset,
+                                family        = family,
+                                treatment     = treatment,
+                                mu0           = mu0,
+                                sigma02       = sigma02,
+                                alpha_max     = alpha_max,
+                                prior.df      = prior.df,
                                 prior.scale   = prior.scale,
-                                prior.mean    = prior.mean, 
-                                number_mcmc   = number_mcmc, 
-                                weibull_shape = weibull_shape, 
+                                prior.mean    = prior.mean,
+                                number_mcmc   = number_mcmc,
+                                weibull_shape = weibull_shape,
+                                weibull_scale = weibull_scale,
+                                two_side      = two_side)
+
+    mu_posterior <- mu_post_aug(Y             = Y,
+                                X             = X,
+                                offset        = offset,
+                                family        = family,
+                                treatment     = treatment,
+                                mu0           = mu0,
+                                sigma02       = sigma02,
+                                alpha_loss    = alpha_loss$alpha_loss,
+                                prior.df      = prior.df,
+                                prior.scale   = prior.scale,
+                                prior.mean    = prior.mean,
+                                number_mcmc   = number_mcmc,
+                                weibull_shape = weibull_shape,
                                 weibull_scale = weibull_scale)
 
-    
+
     return(list(alpha_loss         = alpha_loss$alpha_loss,
                 pvalue             = alpha_loss$pvalue,
                 mu_posterior       = mu_posterior$mu_post,
@@ -318,42 +317,42 @@ setMethod("post_est_gaussian",
                 mu0                = mu0,
                 sigma02            = sigma02))
   }
-  
-  
+
+
   final <- function(posterior) {
     den_post  <- density(posterior$mu_posterior, adjust = 0.5)
     den_flat  <- density(posterior$mu_posterior_flat, adjust = 0.5)
     den_prior <- density(posterior$mu_prior, adjust = 0.5)
-    
+
     post <- posterior$mu_posterior
-    
+
     return(list(den_post  = den_post,
                 den_flat  = den_flat,
                 den_prior = den_prior,
                 post      = post))
   }
-  
-  
+
+
   est <- mu_posterior(
-    Y             = Y, 
-    X             = X, 
-    offset        = offset, 
-    family        = family, 
-    treatment     = treatment, 
-    mu0           = mu0, 
-    sigma02       = sigma02, 
-    alpha_max     = alpha_max, 
-    prior.df      = prior.df, 
-    prior.scale   = prior.scale, 
-    prior.mean    = prior.mean, 
-    number_mcmc   = number_mcmc, 
-    weibull_shape = weibull_shape, 
-    weibull_scale = weibull_scale, 
+    Y             = Y,
+    X             = X,
+    offset        = offset,
+    family        = family,
+    treatment     = treatment,
+    mu0           = mu0,
+    sigma02       = sigma02,
+    alpha_max     = alpha_max,
+    prior.df      = prior.df,
+    prior.scale   = prior.scale,
+    prior.mean    = prior.mean,
+    number_mcmc   = number_mcmc,
+    weibull_shape = weibull_shape,
+    weibull_scale = weibull_scale,
     two_side      = two_side)            #Two or one sided hypothesis test?
 
-  
+
   f1 <- final(posterior = est)
-  
+
   args1 <- list(data          = data,
                 formula       = formula,
                 family        = family,
@@ -371,15 +370,15 @@ setMethod("post_est_gaussian",
                 two_side      = two_side,
                 inequality    = inequality,
                 delta         = delta)
-  
+
   me <- list(est   = est,
              f1    = f1,
              args1 = args1)
-  
-  class(me) <- "post_est_gaussian"
-  
+
+  class(me) <- "bdpregression_linear"
+
   return(me)
-  
+
 })
 
 
@@ -389,19 +388,19 @@ setMethod("post_est_gaussian",
 #' print
 #'
 #' @title print: print
-#' @param x post_est_gaussian
+#' @param x bdpregression_linear
 #'
 #' @examples
 #'
 #' @rdname print
 #' @export print
-setMethod("print", signature(x = "post_est_gaussian"), function(x){
-  
+setMethod("print", signature(x = "bdpregression_linear"), function(x){
+
   f          <- x$f1
   posterior  <- x$est
   inequality <- x$args1$inequality
   delta      <- x$args1$delta
-  
+
   if (inequality == "<") {
     hypothesis <- paste("\"We can define mu as the mean for the test", "\n",
                         "Null Hypothesis (H_0): mu>", delta, "\n",
@@ -410,7 +409,7 @@ setMethod("print", signature(x = "post_est_gaussian"), function(x){
                         "We can accept H_a with a Probability of",
                         mean(f$post < delta))
   }
-  
+
   if (inequality == ">") {
     hypothesis <- paste("\"Define mu as the mean of the data", "\n",
                         "Null Hypothesis (H_0): mu<", delta, "\n",
@@ -419,11 +418,11 @@ setMethod("print", signature(x = "post_est_gaussian"), function(x){
                         "We can accept H_a with a Probability of",
                         mean(f$post > delta))
   }
-  
+
   ### Print
   prior <- list(`Bayesian p-value (new vs historical data)`       = posterior$pvalue,
                 `Loss function value`                             = posterior$alpha_loss)
-  
+
   print(cat(hypothesis))
   print(prior)
 })
@@ -434,50 +433,50 @@ setMethod("print", signature(x = "post_est_gaussian"), function(x){
 #' plot
 #'
 #' @title plot: plot
-#' @param x post_est_gaussian
+#' @param x bdpregression_linear
 #'
 #' @examples
 #'
 #' @rdname plot
 #' @export plot
-setMethod("plot", signature(x = "post_est_gaussian"), function(x){
-  
+setMethod("plot", signature(x = "bdpregression_linear"), function(x){
+
   f          <- x$f1
   posterior  <- x$est
   two_side   <- x$args1$two_side
   inequality <- x$args1$inequality
   delta      <- x$args1$delta
-  
+
   D4 <- data.frame(information_sources = "Posterior",
                    y                   = f$den_post$y,
                    x                   = f$den_post$x)
-  
+
   D5 <- data.frame(information_sources = "Current data",
                    y                   = f$den_flat$y,
                    x                   = f$den_flat$x)
-  
+
   D6 <- data.frame(information_sources = "Prior",
                    y                   = f$den_prior$y,
                    x                   = f$den_prior$x)
-  
+
   D <- as.data.frame(rbind(D4, D5, D6))
-  
+
   D$information_sources <- factor(D$information_sources,
                                   levels = (c("Posterior", "Current data", "Prior")))
-  
+
   post_typeplot <- ggplot(D, aes(x = x, y = y)) +
     geom_line(size = 2, aes(colour = information_sources, lty = information_sources)) +
     theme_bw() +
     ylab("Density (PDF)") +
     xlab("values")
-  
-  
+
+
   densityplot <- ggplot(subset(D, information_sources == "Posterior"), aes(x = x, y = y)) +
     geom_line(size = 2) +
     ylab("Density (PDF)") +
     xlab("values") +
     theme_bw()
-  
+
   if (two_side == 1) {
     p_value = seq(0, 1, , 100)
     p_value = ifelse(p_value > 0.5, 1 - p_value, p_value)
@@ -485,21 +484,21 @@ setMethod("plot", signature(x = "post_est_gaussian"), function(x){
   if (two_side == 0) {
     p_value = seq(0, 1, , 100)
   }
-  
+
   Loss_function <- pweibull(p_value,
                             shape = posterior$weibull_shape,
                             scale = posterior$weibull_scale)
-  
+
   D1 <- data.frame(y = Loss_function, x = seq(0, 1, , 100))
   D2 <- data.frame(pvalue = c(posterior$pvalue))
-  
+
   lossfun_plot <- ggplot() +
     geom_line(data = D1, aes(y = y, x = x), size = 1) +
     geom_vline(data = D2, aes(xintercept = pvalue), lty = 2) +
     theme_bw() +
     ylab("Effective sample size for historical data") +
     xlab("Bayesian p-value (new vs historical data)")
-  
+
   op <- par(ask=TRUE)
   plot(post_typeplot)
   plot(densityplot)
