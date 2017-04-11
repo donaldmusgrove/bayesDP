38dc6469e6a86054df1550380b0c600edacfd66a
Donnie M <donniemusgrove@gmail.com>
2017-04-11 15:16:00 -0500

    Update normal and binomial print methods

--- R/print.R
+++ R/print.R
@@ -7,35 +7,8 @@
 #' @param x Result
 #' @export
 setMethod("print", signature(x = "bdpnormal"), function(x){
-  posterior_treatment <- x$posterior_treatment
-  posterior_control   <- x$posterior_control
-  N0_t                <- x$args1$N0_t
-  N0_c                <- x$args1$N0_c
-
-  if(is.null(N0_t) == FALSE){
-      prior_for_treatment_group <- list("Sample size of prior (for treatment group):  " = N0_t,
-                                        "Bayesian p-value (new vs historical data):  "  = posterior_treatment$p_hat,
-                                        "Discount function value:  "                    = posterior_treatment$alpha_discount)
-      }
-  if(is.null(N0_c) == FALSE){
-      prior_for_control_group <- list("Sample size of prior (for control group):  "  = N0_c,
-                                      "Bayesian p-value (new vs historical data):  " = posterior_control$p_hat,
-                                      "Discount function value:  "                   = posterior_control$alpha_discount)
-      }
-  pp(prior_for_treatment_group)
-  if(is.null(N0_c) == FALSE){
-    pp(prior_for_control_group)
-  }
-
-  argsdf <- suppressWarnings(data.frame(as.numeric(as.character(x$args1))))
-  rownames(argsdf) <- names(x$args1)
-  colnames(argsdf) <- "args"
-  #print(format(head(argsdf, -2), scientific = FALSE))
-  cat("\n")
-  cat("Submitted:")
-  cat(x$args1$intent)
-  cat("\n")
-  invisible(x)
+  ### Return summary
+  summary(x)
 })
 
 
@@ -48,45 +21,8 @@ setMethod("print", signature(x = "bdpnormal"), function(x){
 #' @param x Result
 #' @export
 setMethod("print", signature(x = "bdpbinomial"), function(x){
-  f                   <- x$f1
-  posterior_treatment <- x$posterior_treatment
-  posterior_control   <- x$posterior_control
-  two_side            <- x$args1$two_side
-  N0_t                <- x$args1$N0_t
-  N0_c                <- x$args1$N0_c
-
-
-  if(is.null(N0_t) == FALSE){
-      prior_for_treatment_group <- list(
-        `Sample size of prior (for treatment group):  `          = posterior_treatment$N0,
-        #`Effective sample size of prior(for treatment group):  ` = posterior_treatment$N0_effective,
-        `Bayesian p-value (new vs historical data):  `           = posterior_treatment$p_hat,
-        `Discount function value:  `                             = posterior_treatment$alpha_discount)
-      }
-
-  if(is.null(N0_c) == FALSE){
-
-      prior_for_control_group <- list(
-        `Sample size of prior (for control group):  `          = posterior_control$N0,
-        #`Effective sample size of prior(for control group):  ` = posterior_control$N0_effective,
-        `Bayesian p-value (new vs historical data):  `         = posterior_control$p_hat,
-        `Discount function value:  `                           = posterior_control$alpha_discount)
-      }
-
-  pp(prior_for_treatment_group)
-  if(is.null(N0_c) == FALSE){
-    pp(prior_for_control_group)
-  }
-
-  argsdf <- suppressWarnings(data.frame(as.numeric(as.character(x$args1))))
-  rownames(argsdf) <- names(x$args1)
-  colnames(argsdf) <- "args"
-  print(format(head(argsdf, -2), scientific = FALSE))
-  cat("\n")
-  cat("Submitted:")
-  cat(x$args1$intent)
-  cat("\n")
-  invisible(x)
+  ### Return summary
+  summary(x)
 })
 
 
@@ -134,23 +70,6 @@ setMethod("print", signature(x = "bdpsurvival"), function(x){
     print(print_1arm)
   }
 
-
-
-
-  # ### Format surv_time output
-  # surv_CI    <- round(quantile(f$treatment_posterior, prob=c(0.025, 0.975)),4)
-  # surv_est   <- round(median(f$treatment_posterior),4)
-  # surv_print <- paste0(surv_est, " (",surv_CI[1], ", ", surv_CI[2],")")
-  #
-  # ### Output list
-  # prior_for_treatment_group <- list(`Stochastic comparison (new vs historical data):  ` = posterior_treatment$p_hat,
-  #                                   `Discount function value:  `                        = posterior_treatment$alpha_discount,
-  #                                   `Survival time:  `                                  = surv_time,
-  #                                   `Median survival probability (95% CI):  `           = surv_print)
-  #
-  # ### Text outputs
-  # cat(pp(prior_for_treatment_group))
-  # invisible(x)
 })
 
 
