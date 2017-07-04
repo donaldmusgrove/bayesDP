45696b2488b26e3d941c2000b55e5f78817ea335
Shawn Balcome <balc0022@umn.edu>
2017-07-03 20:24:51 -0500

    Added two modular functions.

---
+++ R/alpha_discount.R
@@ -0,0 +1,86 @@
+
+#' @importFrom ggplot2 aes
+#' @importFrom ggplot2 facet_wrap
+#' @importFrom ggplot2 geom_hline
+#' @importFrom ggplot2 geom_line
+#' @importFrom ggplot2 geom_vline
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 theme_bw
+#' @importFrom ggplot2 xlab
+#' @importFrom ggplot2 ylab
+#' @title INSERT TEXT HERE
+#' @description \code{alpha_discount} INSERT TEXT HERE
+#' @param phat INSERT TEXT HERE
+#' @param alpha_max scalar. Maximum weight the discount function can apply.
+#'   Default is 1. For a two-arm trial, users may specify a vector of two values
+#'   where the first value is used to weight the historical treatment group and
+#'   the second value is used to weight the historical control group.
+#' @param weibull_shape scalar. Shape parameter of the Weibull discount function
+#'   used to compute alpha, the weight parameter of the historical data. Default
+#'   value is 3. For a two-arm trial, users may specify a vector of two values
+#'   where the first value is used to estimate the weight of the historical
+#'   treatment group and the second value is used to estimate the weight of the
+#'   historical control group.
+#' @param weibull_scale scalar. Scale parameter of the Weibull discount function
+#'   used to compute alpha, the weight parameter of the historical data. Default
+#'   value is 0.135. For a two-arm trial, users may specify a vector of two values
+#'   where the first value is used to estimate the weight of the historical
+#'   treatment group and the second value is used to estimate the weight of the
+#'   historical control group.
+#' @details \code{alpha_discount} INSERT TEXT HERE
+#'
+#'   For more details, see the \code{alpha_discount} vignette: \cr
+#'   \code{vignette("alpha_discount-vignette", package="bayesDP")}
+#'
+#'
+#' @return \code{alpha_discount} returns an object of class "alpha_discount".
+#'
+#' An object of class \code{alpha_discount} is a list containing at least
+#' the following components:
+#' \describe{
+#'  \item{\code{posterior_treatment}}{
+#'    list. Entries contain values related to the data:}
+#'    \itemize{
+#'     INSERT TEXT HERE
+#'   }
+#' }
+#'
+#' @seealso \code{\link[=summary,alpha_discount-method]{summary}},
+#'   \code{\link[=print,alpha_discount-method]{print}},
+#'   and \code{\link[=plot,alpha_discount-method]{plot}} for details of each of the
+#'   supported methods.
+#'
+#' @references
+#' Haddad, T., Himes, A., Thompson, L., Irony, T., Nair, R. MDIC Computer
+#'   Modeling and Simulation working group.(2017) Incorporation of stochastic
+#'   engineering models as prior information in Bayesian medical device trials.
+#'   \emph{Journal of Biopharmaceutical Statistics}, 1-15.
+#'
+#' @examples
+#' # INSERT EXAMPLE HERE
+#'
+#' @rdname alpha_discount
+#' @import methods
+#' @importFrom stats sd density is.empty.model median model.offset model.response pweibull quantile rbeta rgamma rnorm var vcov
+#' @aliases alpha_discount,ANY-method
+#' @export alpha_discount
+alpha_discount <- setClass("alpha_discount", slots = c(args1 = "list"))
+
+setGeneric("alpha_discount",
+           function(p_hat          = NULL,
+                    alpha_max     = 1,
+                    weibull_scale = 0.135,
+                    weibull_shape = 3){
+             standardGeneric("alpha_discount")
+           })
+
+setMethod("alpha_discount",
+          signature(),
+          function(p_hat          = NULL,
+                   alpha_max     = 1,
+                   weibull_scale = 0.135,
+                   weibull_shape = 3){
+
+  alpha_hat <- pweibull(p_hat, shape = weibull_shape, scale = weibull_scale) * alpha_max
+  return(alpha_hat)
+})
---
+++ R/probability_discount.R
@@ -0,0 +1,101 @@
+
+#' @importFrom ggplot2 aes
+#' @importFrom ggplot2 facet_wrap
+#' @importFrom ggplot2 geom_hline
+#' @importFrom ggplot2 geom_line
+#' @importFrom ggplot2 geom_vline
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 theme_bw
+#' @importFrom ggplot2 xlab
+#' @importFrom ggplot2 ylab
+#' @title INSERT TEXT HERE
+#' @description \code{probability_discount} INSERT TEXT HERE
+#' @param mu scalar. Mean of the current data.
+#' @param sigma scalar. Standard deviation of the current data.
+#' @param N scalar. Number of observations of the current data.
+#' @param mu0 scalar. Mean of the historical data.
+#' @param sigma0 scalar. Standard deviation of the historical data.
+#' @param N0 scalar. Number of observations of the historical data.
+#' @param number_mcmc scalar. Number of Monte Carlo simulations. Default is 10000.
+#' @param two_side logical. Indicator of two-sided test for the discount
+#'   function. Default value is TRUE.
+#' @details \code{probability_discount} INSERT TEXT HERE
+#'
+#'   For more details, see the \code{probability_discount} vignette: \cr
+#'   \code{vignette("probability_discount-vignette", package="bayesDP")}
+#'
+#'
+#' @return \code{probability_discount} returns an object of class "probability_discount".
+#'
+#' An object of class \code{probability_discount} is a list containing at least
+#' the following components:
+#' \describe{
+#'  \item{\code{posterior_treatment}}{
+#'    list. Entries contain values related to the data:}
+#'    \itemize{
+#'     INSERT TEXT HERE
+#'   }
+#' }
+#'
+#' @seealso \code{\link[=summary,probability_discount-method]{summary}},
+#'   \code{\link[=print,probability_discount-method]{print}},
+#'   and \code{\link[=plot,probability_discount-method]{plot}} for details of each of the
+#'   supported methods.
+#'
+#' @references
+#' Haddad, T., Himes, A., Thompson, L., Irony, T., Nair, R. MDIC Computer
+#'   Modeling and Simulation working group.(2017) Incorporation of stochastic
+#'   engineering models as prior information in Bayesian medical device trials.
+#'   \emph{Journal of Biopharmaceutical Statistics}, 1-15.
+#'
+#' @examples
+#' # INSERT EXAMPLE HERE
+#'
+#' @rdname probability_discount
+#' @import methods
+#' @importFrom stats sd density is.empty.model median model.offset model.response pweibull quantile rbeta rgamma rnorm var vcov
+#' @aliases probability_discount,ANY-method
+#' @export probability_discount
+probability_discount <- setClass("probability_discount", slots = c(args1 = "list"))
+
+setGeneric("probability_discount",
+           function(mu          = NULL,
+                    sigma       = NULL,
+                    N           = NULL,
+                    mu0         = NULL,
+                    sigma0      = NULL,
+                    N0          = NULL,
+                    number_mcmc = 10000,
+                    two_side    = TRUE){
+             standardGeneric("probability_discount")
+           })
+
+setMethod("probability_discount",
+          signature(),
+          function(mu          = NULL,
+                   sigma       = NULL,
+                   N           = NULL,
+                   mu0         = NULL,
+                   sigma0      = NULL,
+                   N0          = NULL,
+                   number_mcmc = 10000,
+                   two_side    = TRUE){
+
+  ### Preposterior of current mu using flat prior
+  sigma2_post_flat <- 1/rgamma(number_mcmc, (N - 1)/2, ((N - 1) * sigma^2)/2)
+  mu_post_flat     <- rnorm(number_mcmc, mu, (sigma2_post_flat/((N-1)+1))^0.5)
+
+  ### Posterior of historical data parameters using flat prior
+  sigma2_post_flat0 <- 1/rgamma(number_mcmc, (N0-1)/2, ((N0-1)*sigma0^2)/2)
+  mu_post_flat0     <- rnorm(number_mcmc, mu0, (sigma2_post_flat0/((N0-1)+1))^0.5)
+
+  ### Test of model vs real
+  p_hat <- mean(mu_post_flat < mu_post_flat0)
+
+  ### Check if two-sided and transform p_hat accordingly
+  if(two_side){
+    p_hat <- ifelse(p_hat > 0.5, 1 - p_hat, p_hat)
+  }
+
+  return(p_hat)
+})
