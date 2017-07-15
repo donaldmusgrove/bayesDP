97534fce164814c951bb44a9d3097aa24d23cffa
Donnie M <donniemusgrove@gmail.com>
2017-07-15 10:26:52 -0500

    Added bdplm functions to bayesDP-init file

--- src/bayesDP-init.c
+++ src/bayesDP-init.c
@@ -6,10 +6,16 @@
 
 extern SEXP bayesDP_ppexpM(SEXP, SEXP, SEXP);
 extern SEXP bayesDP_ppexpV(SEXP, SEXP, SEXP);
+extern SEXP bayesDP_sigma2marginal(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP bayesDP_betaRegSampler(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+
 
 static const R_CallMethodDef CallEntries[] = {
   {"bayesDP_ppexpM", (DL_FUNC) &bayesDP_ppexpM, 3},
   {"bayesDP_ppexpV", (DL_FUNC) &bayesDP_ppexpV, 3},
+  {"bayesDP_sigma2marginal", (DL_FUNC) &bayesDP_sigma2marginal, 8},
+  {"bayesDP_betaRegSampler", (DL_FUNC) &bayesDP_betaRegSampler, 6},
+
   {NULL, NULL, 0}
 };
 
